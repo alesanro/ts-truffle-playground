46a2d90821bc9cbd1ec7c6fb1428740ecc9809d5
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-08-17 16:22:49 +0300

    Add truffle definition

--- test/fakecoin.ts
+++ test/fakecoin.ts
@@ -1,19 +1,31 @@
-const FakeCoin = artifacts.require("FakeCoin")
+
+
+import Web3 = require('web3')
+import { ContractContextDefinition } from 'truffle';
+
+declare const web3: Web3;
+
+// declare const artifacts: SignHashArtifacts;
+// declare const contract: ContractContextDefinition;
+
+// const FakeCoin = artifacts.require("FakeCoin")
+
+declare const contract: ContractContextDefinition;
 
 import { assert } from "chai";
 
 contract("FakeCoin", accounts => {
 
-	let coin: FakeCoinInstance
+	// let coin: FakeCoinInstance
 
 	before(async () => {
-		coin = await FakeCoin.deployed()
+		// coin = await FakeCoin.deployed()
 	})
 
 	describe("initial state", () => {
 
 		it("read-only getters", async () => {
-			assert.equal((await coin.totalSupply()).toString(16), "0", "Initial totalSupply should be 0")
+			// assert.equal((await coin.totalSupply()).toString(16), "0", "Initial totalSupply should be 0")
 		})
 	})
 })
--- truffle.d.ts
+++ truffle.d.ts
@@ -1,19 +1,84 @@
-declare type _contractTest = (accounts: string[]) => void;
-declare type _testCaseBody = () => Promise<void>
-declare function contract(name: string, test: _contractTest): void;
 
-declare interface Contract<T> {
-  "new"(): Promise<T>,
-  deployed(): Promise<T>,
-  at(address: string): T,
+declare type Callback<T> = (err: Error | null, value: T) => void;
+
+declare type Address = string;
+
+declare module '*.json' {
+  const value: any;
+  export default value;
 }
 
-declare interface FakeCoinInstance {
-	totalSupply(): Promise<number>;
-}
+declare module 'truffle' {
+  import { Tx } from 'web3/eth/types';
 
-interface Artifacts {
-  require(name: "FakeCoin"): Contract<FakeCoinInstance>,
-}
+  namespace truffle {
+    
+    type ContractCallback = (
+      this: Mocha.ISuiteCallbackContext,
+      accounts: Address[]
+    ) => void;
 
-declare var artifacts: Artifacts;
+    type ContractContextDefinition = (
+      description: string,
+      callback: ContractCallback
+    ) => Mocha.ISuite;
+
+    interface ContractBase {
+      address: Address;
+      sendTransaction(Tx: Tx): Promise<TransactionResult>;
+    }
+
+    interface Contract<T> extends ContractBase {
+      at(address: Address): Promise<T>;
+      deployed(): Promise<T>;
+    }
+
+    interface AnyContract extends Contract<any> {
+      'new'(...args: any[]): Promise<any>;
+    }
+
+    interface TruffleArtifacts {
+      require(name: string): AnyContract;
+    }
+
+    type TransactionReceipt = {
+      transactionHash: string;
+      transactionIndex: number;
+      blockHash: string;
+      blockNumber: number;
+      gasUsed: number;
+      cumulativeGasUsed: number;
+      contractAddress: Address | null;
+      logs: [TransactionLog];
+    };
+
+    type TransactionLog = {
+      logIndex: number;
+      transactionIndex: number;
+      transactionHash: string;
+      blockHash: string;
+      blockNumber: number;
+      address: Address;
+      type: string;
+      event: string;
+      args: any;
+    };
+
+    type TransactionResult = {
+      tx: string;
+      receipt: TransactionReceipt;
+      logs: [TransactionLog];
+    };
+
+    interface Deployer extends Promise<void> {
+      deploy(object: ContractBase, ...args: any[]): Promise<void>;
+
+      link(
+        library: ContractBase,
+        contracts: ContractBase | [ContractBase]
+      ): Promise<void>;
+    }
+  }
+
+  export = truffle;
+}
