0dc2933d9747eb384b751d21da264d2758ea2631
Alex Rudyak <aliaksandr.rudziak@instinctools.ru>
2018-08-23 17:05:16 +0300

    Update artifacts-export script

--- scripts/artifacts-export/cli.ts
+++ scripts/artifacts-export/cli.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
 import * as _ from 'lodash';
-import * as Handlebars from 'handlebars'
+import * as Handlebars from 'handlebars';
 import * as yargs from 'yargs';
-import { pascalize } from 'humps'
-import { sync as globSync } from 'glob'
+import chalk from 'chalk';
+import { pascalize } from 'humps';
+import { sync as globSync } from 'glob';
 
 import { readdirSync, readFileSync, writeFileSync } from "fs";
-import * as path from 'path'
+import * as path from 'path';
 
-const DEFAULT_CONTRACT_POSTFIX = 'Instance'
-const ALLOWED_EXTENSIONS = ['.ts']
-const ARTIFACTS_EXPORT_TEMPLATE_FILE = 'artifacts-export-template.handlebars'
-const CONTRACTS_EXPORT_TEMPLATE_FILE = 'contracts-export-template.handlebars'
+const DEFAULT_CONTRACT_POSTFIX = 'Instance';
+const ALLOWED_EXTENSIONS = ['.ts'];
+const ARTIFACTS_EXPORT_TEMPLATE_FILE = 'artifacts-export-template.handlebars';
+const CONTRACTS_EXPORT_TEMPLATE_FILE = 'contracts-export-template.handlebars';
 
 interface TemplateContext {
     contracts: Contract[];
@@ -69,7 +69,7 @@ const args = yargs
     .options('contractNames', {
         alias: ['cn'],
         describe: 'List of contract names to verify correctnes of contracts',
-        type: 'array'
+        type: 'array',
     })
     .conflicts('artifacts', 'contractNames')
     .example(
@@ -82,70 +82,94 @@ const args = yargs
     )
     .argv;
 
+// ------------ get input generated contract files ----------------------
+const inputFiles: string[] = readdirSync(args.inputFolder, "utf8");
 
-const files: string[] = readdirSync(args.inputFolder, "utf8")
-
-if (_.isEmpty(files)) {
-    console.log(`${chalk.red(`No contracts found in ${args.inputFolder} folder`)}`)
-    console.log(`Please make sure that you've generated any contracts first.`)
-    process.exit(1)
+if (_.isEmpty(inputFiles)) {
+    console.log(`${chalk.red(`No contracts found in ${args.inputFolder} folder`)}`);
+    console.log(`Please make sure that you've generated any contracts first.`);
+    process.exit(1);
 }
 
-console.log(`${chalk.magenta(`Hey, found some contracts:`)}`)
 
-const excludePatters = (args.exclude as string[]) || []
-const excludedFiles = _.map(excludePatters, (pattern) => {
-    console.log(`Perform search pattern to exclude: ${pattern}`)
-    return globSync(pattern, { cwd: args.inputFolder })
-})
-const excludedFilesFlatten = _.flatten(excludedFiles)
-console.log(`Files excluded: ${excludedFilesFlatten.length > 0 ? chalk.redBright(`${excludedFilesFlatten}`) : `no files found`}.`)
+// ------------------- get excluded files ---------------------
+const excludedFiles = getExcludedFiles(args.inputFolder, args.exclude)
+console.log(`Files excluded: ${excludedFiles.length > 0 ? chalk.redBright(`${excludedFiles}`) : `no files found`}.`)
 
-const contractPostfix = pascalize(args.postfix || DEFAULT_CONTRACT_POSTFIX)
-const contracts: Contract[] = _.filter(files, file => {
+
+// ----------- get the rest of the files -----------------
+const gotContracts: string[] = getProvidedContractNames(args.artifacts, args.contractNames)
+const includedFiles = _.filter(inputFiles, file => {
     return _.includes(ALLOWED_EXTENSIONS, path.extname(file)) && 
-        !_.includes(excludedFilesFlatten, file)
+    !_.includes(excludedFiles, file);
 })
-.map((file, index) => {
-    let filename = path.basename(file, path.extname(file))
-    let contractArtifactName = pascalize(`${filename}`)
-    let contractInstanceName = pascalize(`${filename}${contractPostfix}`)
-    console.log(`${index + 1}: ${chalk.underline(`${file}`)} => ${chalk.greenBright(`${contractInstanceName}`)}`)
+const artifactNames = _.map(includedFiles, file => {
+    const filename = path.basename(file, path.extname(file))
+    const artifactName = pascalize(`${filename}`)
+    return artifactName
+})
 
+// ----------- get selected by user contracts -----------------
+const contractPostfix = pascalize(args.postfix || DEFAULT_CONTRACT_POSTFIX)
+const contracts: Contract[] = _.zipWith<string, string, [string, string]>(includedFiles, artifactNames, (v1, v2) => [v1, v2])
+.filter(([, artifactName])  => {
+    return _.includes(gotContracts, artifactName)
+})
+.map(([file, artifactName]) => {
+    const filename = path.basename(file, path.extname(file))
+    const contractInstanceName = pascalize(`${artifactName}${contractPostfix}`);
     return <Contract>{
         filename: filename,
-        contractArtifactName: contractArtifactName,
+        contractArtifactName: artifactName,
         contractInstanceName: contractInstanceName,
         contractLibRelativePath: `./${filename}`,
     };
 })
 
-console.log(`${chalk.greenBright(`Found contracts:`)}`)
+if (_.isEmpty(contracts)) {
+    console.log(`${chalk.red(`Cannot receive contracts that are available for exporting (neither excluded or not included)`)}`)
+    process.exit(1)
+}
+
+console.log(`${chalk.underline(`Got contracts:`)}`)
 _.forEach(contracts, (contract) => {
-    console.log(`${JSON.stringify(contract, null, 4)}`)
+    console.log(`- ${chalk.gray(contract.filename)} => ${chalk.greenBright(`${contract.contractArtifactName}(${contract.contractInstanceName})`)}`)
 })
 
+/// ----------- initialize template context ----------
 const templateContext = <TemplateContext>{
     contracts: contracts,
     generatedDir: `./${path.relative(args.output, args.inputFolder)}`
 }
 
-
-/// ------------ load templates ------------
-
-const artifactsTemplatePath = path.resolve(path.join(args.templates, ARTIFACTS_EXPORT_TEMPLATE_FILE))
-const artifactsTemplateContent = getNamedContent(artifactsTemplatePath)
-const artifactsTemplate = Handlebars.compile<TemplateContext>(artifactsTemplateContent.content)
-const renderedArtifactsTemplate = artifactsTemplate(templateContext)
-writeFileSync(path.join(args.output, 'artifacts.d.ts'), renderedArtifactsTemplate)
+/// ------------ fill CONTRACTS templates ------------
+console.log(`Start writes of ${chalk.bold(`contracts`)} exports...`)
 
 const contractsTemplatePath = path.resolve(path.join(args.templates, CONTRACTS_EXPORT_TEMPLATE_FILE))
 const contractsTemplateContent = getNamedContent(contractsTemplatePath)
 const contractsTemplate = Handlebars.compile<TemplateContext>(contractsTemplateContent.content)
 const renderedContractsTemplate = contractsTemplate(templateContext)
-writeFileSync(path.join(args.inputFolder, 'index.ts'), renderedContractsTemplate)
+
+const contractsOutputPath = path.join(args.inputFolder, 'index.ts');
+writeFileSync(contractsOutputPath, renderedContractsTemplate)
+
+console.log(`${chalk.bold(`Contracts`)} exports successfully was written to ${chalk.underline(`${path.resolve(contractsOutputPath)}`)}`)
+
+/// ------------ fill ARTIFACTS templates ------------
+console.log(`Start writes of ${chalk.bold(`artifacts`)} exports...`)
+
+const artifactsTemplatePath = path.resolve(path.join(args.templates, ARTIFACTS_EXPORT_TEMPLATE_FILE))
+const artifactsTemplateContent = getNamedContent(artifactsTemplatePath)
+const artifactsTemplate = Handlebars.compile<TemplateContext>(artifactsTemplateContent.content)
+const renderedArtifactsTemplate = artifactsTemplate(templateContext)
+
+const artifactsOutputPath = path.join(args.output, 'artifacts.d.ts');
+writeFileSync(artifactsOutputPath, renderedArtifactsTemplate)
+
+console.log(`${chalk.bold(`Artifacts`)} exports successfully was written to ${chalk.underline(`${path.resolve(artifactsOutputPath)}`)}`)
 
 /// ----------------------------------------
+/// ----------- local function -------------
 
 function getNamedContent(filename: string): { name: string; content: string } {
     const name = path.basename(filename);
@@ -160,3 +184,25 @@ function getNamedContent(filename: string): { name: string; content: string } {
     }
 }
 
+function getExcludedFiles(folder: string, patterns: string[]): string[] {
+    const excludePatters = patterns || [];
+    const excludedFiles = _.map(excludePatters, (pattern) => {
+        console.log(`Perform search pattern to exclude: ${pattern}`)
+        return globSync(pattern, { cwd: folder })
+    })
+    return _.flatten(excludedFiles)
+}
+
+function getProvidedContractNames(artifactsPath: string, contractsList: string[]): string[] | never {
+    if (!_.isEmpty(args.artifacts)) {
+        const contractArtifactsFiles = readdirSync(args.artifacts, "utf8");
+        return _.map(contractArtifactsFiles, file => path.basename(file, path.extname(file)))
+    }
+    else if (!_.isEmpty(args.contractNames)) {
+        return args.contractNames
+    }
+
+    console.log(`${chalk.red(`No contracts passed with "--artifacts" or "--contractNames" params`)}`)
+    console.log(`Please make sure that you provide path to artifacts or list of smart contracts.`)
+    return process.exit(1)
+}
