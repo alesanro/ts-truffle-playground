e26afe7510dca93e30848d74d640e3bb953d4f42
Alex Rudyak <al.rudyak@gmail.com>
2018-10-02 00:56:17 +0300

    Update generated contracts

--- src/generated/contracts/fake_coin.ts
+++ src/generated/contracts/fake_coin.ts
@@ -1,7 +1,7 @@
 // tslint:disable:no-consecutive-blank-lines ordered-imports align trailing-comma whitespace class-name
 // tslint:disable:no-unused-variable
 import BigNumber from "bignumber.js";
-import { TxData as Tx, DecodedLogArgs } from "web3";
+import { TxData, TxDataPayable, CallData, BlockParamLiteral, DecodedLogArgs, EventFilterObject, EventFilterResult } from "web3";
 import { DeployedContract, TransactionResult, Request } from 'truffle-contract';
 // tslint:enable:no-unused-variable
 
@@ -12,91 +12,91 @@ declare namespace FakeCoinInstanceInternal {
     namespace balanceOf {
     
     	interface TransactionCallable<T> {
-    		(index_0: string,  tx?: Tx): Promise<T>;
-    		call(index_0: string,  tx?: Tx): Promise<T>;
-    		sendTransaction(index_0: string,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(index_0: string,  tx?: Tx): Promise<number>;
-    		request(index_0: string,  tx?: Tx): Promise<Request>;
+    		(index_0: string, tx?: CallData): Promise<T>;
+    		call(index_0: string, tx?: CallData): Promise<T>;
+    		sendTransaction(index_0: string, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(index_0: string, tx?: TxData): Promise<number>;
+    		request(index_0: string, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace mint {
     
     	interface TransactionCallable<T> {
-    		(_to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		call(_to: string, _value: BigNumber,  tx?: Tx): Promise<T>;
-    		sendTransaction(_to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(_to: string, _value: BigNumber,  tx?: Tx): Promise<number>;
-    		request(_to: string, _value: BigNumber,  tx?: Tx): Promise<Request>;
+    		(_to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		call(_to: string, _value: BigNumber, tx?: CallData): Promise<T>;
+    		sendTransaction(_to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(_to: string, _value: BigNumber, tx?: TxData): Promise<number>;
+    		request(_to: string, _value: BigNumber, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace totalSupply {
     
     	interface TransactionCallable<T> {
-    		( tx?: Tx): Promise<T>;
-    		call( tx?: Tx): Promise<T>;
-    		sendTransaction( tx?: Tx): Promise<TransactionResult>;
-    		estimateGas( tx?: Tx): Promise<number>;
-    		request( tx?: Tx): Promise<Request>;
+    		(tx?: CallData): Promise<T>;
+    		call(tx?: CallData): Promise<T>;
+    		sendTransaction(tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(tx?: TxData): Promise<number>;
+    		request(tx?: TxData): Promise<Request>;
     	}
     }    
     namespace symbol {
     
     	interface TransactionCallable<T> {
-    		( tx?: Tx): Promise<T>;
-    		call( tx?: Tx): Promise<T>;
-    		sendTransaction( tx?: Tx): Promise<TransactionResult>;
-    		estimateGas( tx?: Tx): Promise<number>;
-    		request( tx?: Tx): Promise<Request>;
+    		(tx?: CallData): Promise<T>;
+    		call(tx?: CallData): Promise<T>;
+    		sendTransaction(tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(tx?: TxData): Promise<number>;
+    		request(tx?: TxData): Promise<Request>;
     	}
     }    
     namespace decimals {
     
     	interface TransactionCallable<T> {
-    		( tx?: Tx): Promise<T>;
-    		call( tx?: Tx): Promise<T>;
-    		sendTransaction( tx?: Tx): Promise<TransactionResult>;
-    		estimateGas( tx?: Tx): Promise<number>;
-    		request( tx?: Tx): Promise<Request>;
+    		(tx?: CallData): Promise<T>;
+    		call(tx?: CallData): Promise<T>;
+    		sendTransaction(tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(tx?: TxData): Promise<number>;
+    		request(tx?: TxData): Promise<Request>;
     	}
     }    
     namespace transfer {
     
     	interface TransactionCallable<T> {
-    		(_to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		call(_to: string, _value: BigNumber,  tx?: Tx): Promise<T>;
-    		sendTransaction(_to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(_to: string, _value: BigNumber,  tx?: Tx): Promise<number>;
-    		request(_to: string, _value: BigNumber,  tx?: Tx): Promise<Request>;
+    		(_to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		call(_to: string, _value: BigNumber, tx?: CallData): Promise<T>;
+    		sendTransaction(_to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(_to: string, _value: BigNumber, tx?: TxData): Promise<number>;
+    		request(_to: string, _value: BigNumber, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace transferFrom {
     
     	interface TransactionCallable<T> {
-    		(_from: string, _to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		call(_from: string, _to: string, _value: BigNumber,  tx?: Tx): Promise<T>;
-    		sendTransaction(_from: string, _to: string, _value: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(_from: string, _to: string, _value: BigNumber,  tx?: Tx): Promise<number>;
-    		request(_from: string, _to: string, _value: BigNumber,  tx?: Tx): Promise<Request>;
+    		(_from: string, _to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		call(_from: string, _to: string, _value: BigNumber, tx?: CallData): Promise<T>;
+    		sendTransaction(_from: string, _to: string, _value: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(_from: string, _to: string, _value: BigNumber, tx?: TxData): Promise<number>;
+    		request(_from: string, _to: string, _value: BigNumber, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace getAccount {
     
     	interface TransactionCallable<T> {
-    		(account: string,  tx?: Tx): Promise<T>;
-    		call(account: string,  tx?: Tx): Promise<T>;
-    		sendTransaction(account: string,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(account: string,  tx?: Tx): Promise<number>;
-    		request(account: string,  tx?: Tx): Promise<Request>;
+    		(account: string, tx?: CallData): Promise<T>;
+    		call(account: string, tx?: CallData): Promise<T>;
+    		sendTransaction(account: string, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(account: string, tx?: TxData): Promise<number>;
+    		request(account: string, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace balanceEth {
     
     	interface TransactionCallable<T> {
-    		(_address: string,  tx?: Tx): Promise<T>;
-    		call(_address: string,  tx?: Tx): Promise<T>;
-    		sendTransaction(_address: string,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(_address: string,  tx?: Tx): Promise<number>;
-    		request(_address: string,  tx?: Tx): Promise<Request>;
+    		(_address: string, tx?: CallData): Promise<T>;
+    		call(_address: string, tx?: CallData): Promise<T>;
+    		sendTransaction(_address: string, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(_address: string, tx?: TxData): Promise<number>;
+    		request(_address: string, tx?: TxData): Promise<Request>;
     	}
     }}
 
@@ -124,6 +124,8 @@ export interface FakeCoinInstance extends DeployedContract {
     getAccount: FakeCoinInstanceInternal.getAccount.TransactionCallable<[string, BigNumber]
 >;
     balanceEth: FakeCoinInstanceInternal.balanceEth.TransactionCallable<BigNumber>;
+
+    Transfer(value: BlockParamLiteral | EventFilterObject): EventFilterResult<FakeCoinTransferEventArgs>;
 }
 
 // tslint:disable:max-file-line-count
--- src/generated/contracts/migrations.ts
+++ src/generated/contracts/migrations.ts
@@ -1,7 +1,7 @@
 // tslint:disable:no-consecutive-blank-lines ordered-imports align trailing-comma whitespace class-name
 // tslint:disable:no-unused-variable
 import BigNumber from "bignumber.js";
-import { TxData as Tx, DecodedLogArgs } from "web3";
+import { TxData, TxDataPayable, CallData, BlockParamLiteral, DecodedLogArgs, EventFilterObject, EventFilterResult } from "web3";
 import { DeployedContract, TransactionResult, Request } from 'truffle-contract';
 // tslint:enable:no-unused-variable
 
@@ -12,41 +12,41 @@ declare namespace MigrationsInstanceInternal {
     namespace last_completed_migration {
     
     	interface TransactionCallable<T> {
-    		( tx?: Tx): Promise<T>;
-    		call( tx?: Tx): Promise<T>;
-    		sendTransaction( tx?: Tx): Promise<TransactionResult>;
-    		estimateGas( tx?: Tx): Promise<number>;
-    		request( tx?: Tx): Promise<Request>;
+    		(tx?: CallData): Promise<T>;
+    		call(tx?: CallData): Promise<T>;
+    		sendTransaction(tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(tx?: TxData): Promise<number>;
+    		request(tx?: TxData): Promise<Request>;
     	}
     }    
     namespace owner {
     
     	interface TransactionCallable<T> {
-    		( tx?: Tx): Promise<T>;
-    		call( tx?: Tx): Promise<T>;
-    		sendTransaction( tx?: Tx): Promise<TransactionResult>;
-    		estimateGas( tx?: Tx): Promise<number>;
-    		request( tx?: Tx): Promise<Request>;
+    		(tx?: CallData): Promise<T>;
+    		call(tx?: CallData): Promise<T>;
+    		sendTransaction(tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(tx?: TxData): Promise<number>;
+    		request(tx?: TxData): Promise<Request>;
     	}
     }    
     namespace setCompleted {
     
     	interface TransactionCallable<T> {
-    		(completed: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		call(completed: BigNumber,  tx?: Tx): Promise<T>;
-    		sendTransaction(completed: BigNumber,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(completed: BigNumber,  tx?: Tx): Promise<number>;
-    		request(completed: BigNumber,  tx?: Tx): Promise<Request>;
+    		(completed: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		call(completed: BigNumber, tx?: CallData): Promise<T>;
+    		sendTransaction(completed: BigNumber, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(completed: BigNumber, tx?: TxData): Promise<number>;
+    		request(completed: BigNumber, tx?: TxData): Promise<Request>;
     	}
     }    
     namespace upgrade {
     
     	interface TransactionCallable<T> {
-    		(new_address: string,  tx?: Tx): Promise<TransactionResult>;
-    		call(new_address: string,  tx?: Tx): Promise<T>;
-    		sendTransaction(new_address: string,  tx?: Tx): Promise<TransactionResult>;
-    		estimateGas(new_address: string,  tx?: Tx): Promise<number>;
-    		request(new_address: string,  tx?: Tx): Promise<Request>;
+    		(new_address: string, tx?: TxData): Promise<TransactionResult>;
+    		call(new_address: string, tx?: CallData): Promise<T>;
+    		sendTransaction(new_address: string, tx?: TxData): Promise<TransactionResult>;
+    		estimateGas(new_address: string, tx?: TxData): Promise<number>;
+    		request(new_address: string, tx?: TxData): Promise<Request>;
     	}
     }}
 
@@ -56,6 +56,7 @@ export interface MigrationsInstance extends DeployedContract {
     owner: MigrationsInstanceInternal.owner.TransactionCallable<string>;
     setCompleted: MigrationsInstanceInternal.setCompleted.TransactionCallable<void>;
     upgrade: MigrationsInstanceInternal.upgrade.TransactionCallable<void>;
+
 }
 
 // tslint:disable:max-file-line-count
